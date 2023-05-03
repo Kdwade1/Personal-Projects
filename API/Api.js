7f4b556a341f8788cf3c8335322289c71575c7b3
Kelvin <kelvin.daquon.wade@gmail.com>
2023-05-02 19:50:32 -0500

    learning node

--- API/Api.js
+++ API/Api.js
@@ -1,4 +1,7 @@
 const api_Url= 'https://api.wheretheiss.at/v1/satellites/25544'
+
+let fistTime= true;
+
 //making a map with tile
 const map = L.map('map').setView([0, 0],0);
 const deathIcon = L.icon({
@@ -11,6 +14,7 @@ const marker = L.marker([0, 0],{icon:deathIcon}).addTo(map);
 
 
 async  function getIss(){
+
     const response =await fetch(api_Url)
     const data= await response.json();
 
@@ -21,12 +25,17 @@ async  function getIss(){
 
     // const marker = L.marker([latitude, longitude]).addTo(map);
     marker.setLatLng([latitude,longitude])
-    document.getElementById('lat').textContent=latitude;
-    document.getElementById('long').textContent=longitude;
+    if(fistTime) {
+        map.setView([latitude, longitude], 2)
+        firstTime= false;
+    }
+    document.getElementById('lat').textContent=latitude.toFixed();
+    document.getElementById('long').textContent=longitude.toFixed();
 }
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 getIss();
+setInterval(getIss,1000)
 
