b8b1f4f84ce4bf0736be93514db3219912b94c4f
Kelvin <kelvin.daquon.wade@gmail.com>
2023-05-15 18:46:18 -0500

    finished player

---
+++ Platformer/Javascript/index.js
@@ -0,0 +1,21 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+canvas.width = 64 * 16;//1024
+canvas.height = 64 * 9;//576
+
+
+
+const player = new Player()
+
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    player.draw()
+    player.update()
+
+}
+
+animate()
