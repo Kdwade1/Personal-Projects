315cb154242cfb2e77afaba512b1ed0ce212f003
Kelvin <kelvin.daquon.wade@gmail.com>
2023-07-05 02:02:44 -0500

    rock paper scissor works

---
+++ Generic Webpages/Rumble/html/rumble.js
@@ -0,0 +1,63 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const gameContainer = document.querySelector('.container');
+    const userResult = document.querySelector('.user_input img');
+    const cpuResult = document.querySelector('.cpu_result img');
+    const result = document.querySelector('.result');
+    const option = document.querySelectorAll('.optional_image');
+
+    option.forEach((image, index) => {
+        image.addEventListener("click", (e) => {
+            image.classList.add("active");
+
+            option.forEach((image2, index2) => {
+                if (index !== index2) {
+                    image2.classList.remove("active");
+                }
+            });
+
+            let imageSrc = e.target.querySelector("img").src;
+
+            userResult.src = imageSrc
+
+
+            let randomNumber = Math.floor(Math.random()* 3)
+
+
+            let cpuImages = ["../img/rock.png", "../img/Paper.png", "../img/scissor.png"];
+
+            cpuResult.src= cpuImages[randomNumber]
+
+
+
+            let cpuValue=["R","P","S"][randomNumber]
+
+            let userValue =["R","P","S"][index]
+
+            let outcome={
+                RR: 'Draw',
+                RP:"CPU",
+                RS:"User",
+                PP:"DRAW",
+                PS:"CPU",
+                PR:"User",
+                SS:"Draw",
+                SR:"CPU",
+                SP:"USER"
+
+            }
+
+            let outcomeValue=outcome[userValue+cpuValue]
+
+
+            result.textContent=userValue ===cpuValue ? "Match Draw" : `${outcomeValue} Won!!`
+
+
+            console.log(cpuValue)
+
+
+
+        });
+    });
+});
