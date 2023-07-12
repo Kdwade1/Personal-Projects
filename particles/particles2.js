6a88ee775081c99a9dff5ecffd7328bb3fdec248
Kelvin <kelvin.daquon.wade@gmail.com>
2023-07-12 12:53:27 -0500

    drew a gradient line to show the color changes

--- particles/particles2.js
+++ particles/particles2.js
@@ -5,13 +5,31 @@ const ctx = canvas.getContext('2d')//<---- provides the 2d rendering context for
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.lineWidth = 5
-const gradient= ctx.createLinearGradient(0,0,canvas.width,canvas.height)
+const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)
 
-gradient.addColorStop(0,'crimson')
-gradient.addColorStop(0.5,'purple')
-gradient.addColorStop(1,'indigo')
-ctx.fillStyle=gradient
-ctx.strokeStyle ='green'
+gradient.addColorStop(0, 'red')
+gradient.addColorStop(0.2, 'orange')
+gradient.addColorStop(0.4, 'yellow')
+gradient.addColorStop(0.6, 'green')
+gradient.addColorStop(0.8, 'blue')
+gradient.addColorStop(1, 'indigo')
+ctx.fillStyle = gradient
+ctx.strokeStyle = gradient
+
+// ctx.beginPath()
+// ctx.moveTo(2,100)
+// ctx.lineTo(100,100)
+//
+// ctx.stroke()
+
+
+ctx.beginPath()
+ctx.moveTo(0, 12)
+ctx.lineTo(40, 60)
+
+ctx.stroke()
+ctx.closePath()
+
 
 // ctx.strokeStyle ='hsl(' +Math.random*360+',100%,50%)'
 
@@ -20,12 +38,11 @@ class Particle {
 
     constructor(effect) {
         this.effect = effect;
-        this.radius = Math.random()*5 +2;
+        this.radius = Math.random() * 9 + 4;
         this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
         this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2);
-        this.vx = Math.random() *4- 2; // Random velocity between -1 and 1
-        this.vy = Math.random()*4- 2
-
+        this.vx = Math.random() * 8 - 2; // Random velocity between -1 and 1
+        this.vy = Math.random() * 8 - 2
 
 
     }
@@ -39,13 +56,14 @@ class Particle {
         // context.stroke();
 
     }
-    update(){
-        this.x+= this.vx
-        if(this.x>this.effect.width -this.radius|| this.x< this.radius)this.vx *=-1
+
+    update() {
+        this.x += this.vx
+        if (this.x > this.effect.width - this.radius || this.x < this.radius) this.vx *= -1
 
 
-        this.y +=this.vy
-        if(this.y>this.effect.width -this.radius|| this.y< this.radius)this.vy *=-1
+        this.y += this.vy
+        if (this.y > this.effect.width - this.radius || this.y < this.radius) this.vy *= -1
     }
 
 }
@@ -82,15 +100,15 @@ class Effect {
     connectParticles(context) {
         const maxDistance = 100
         for (let i = 0; i < this.particles.length; i++) {
-            for (let j = i+1; j < this.particles.length; j++) {
+            for (let j = i + 1; j < this.particles.length; j++) {
                 const dx = this.particles[i].x - this.particles[j].x
                 const dy = this.particles[i].y - this.particles[j].y
                 const distance = Math.hypot(dx, dy)
 
                 if (distance < maxDistance) {
                     context.save()
-                    const opacity=1-(distance/maxDistance)
-                    context.globalAlpha =opacity
+                    const opacity = 1 - (distance / maxDistance)
+                    context.globalAlpha = opacity
                     context.beginPath();
                     context.moveTo(this.particles[i].x, this.particles[i].y)
                     context.lineTo(this.particles[j].x, this.particles[j].y)
@@ -107,12 +125,19 @@ class Effect {
 }
 
 const effect = new Effect(canvas)
+
 // console.log(effect)
 
 
-
 function animate() {
-    ctx.clearRect(0,0,canvas.width,canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    ctx.beginPath()
+    ctx.moveTo(0, 0)
+    ctx.lineTo(canvas.width, canvas.height)
+
+    ctx.stroke()
+
     effect.handleParticles(ctx)
     requestAnimationFrame(animate)
 }
